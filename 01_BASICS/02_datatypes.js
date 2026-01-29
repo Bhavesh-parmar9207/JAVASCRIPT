1e04d74740eac23fd4ba3ae3906a7bd42d3c8820
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-01-29 23:30:06 +0530

    Datatype Learing

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//it's for web-console not for nodejs
+
+//tc39
+//mdn for knowledge 
+
+let name = "Bhavesh";//string datatype
+let age = 20;//number datatype
+let isLoggedIn = true;//boolean datatype
+
+
+//number => 2 to power 53
+// bignit for very large number
+//string => " "
+//null => empty/Standalone value
+//undefined => value not assigned
+//symbol => unique identifier
+
+//undefined is individual datatype
+//null is type of object
+
+//object  
+
+//typeof operator
+console.log(typeof null);//o/p => object
+console.log(typeof undefined);//o/p => undefined
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
