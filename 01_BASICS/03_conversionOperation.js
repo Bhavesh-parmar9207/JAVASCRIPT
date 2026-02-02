519263613b579c909992ea2dcfdbe3ab9e28ce0e
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-02-02 15:00:31 +0530

    Consversion of DataTypes

---
+++ 01_BASICS/03_conversionOperation.js
@@ -0,0 +1,42 @@
+// let score = null
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score); // conversion from string to number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+/*
+"12" => 12
+"12abc" => NaN
+null => 0
+undefined => NaN
+in boolean conversion{
+  true => 1
+  false => 0
+}
+*/
+
+
+
+
+//1 to boolean conversion
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+/*
+1 => true; 0 => false
+"" => false
+"abc" => true
+
+*/
+
+
+
+//num to String
+let someNumber = 11
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
