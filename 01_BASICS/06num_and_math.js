cb6b3e7c31ddecf598133c52d5ed825a0796b15d
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-02-04 22:12:57 +0530

    nums little learning

---
+++ 01_BASICS/06num_and_math.js
@@ -0,0 +1,16 @@
+const score = 100
+console.log(score);
+
+
+const balance = new Number(100);//object type
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));//1
+
+const otherNumber = 125.8966
+console.log(otherNumber.toPrecision(3));//use =>  it gives you roundoff the value
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-In'))// => to give indian standerd type method
+
