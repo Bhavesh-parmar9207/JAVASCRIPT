33292324d90700746fe2861d0006d1d3e19408d9
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-02-09 23:32:35 +0530

    array and object via functions

--- 03_BASICS/01_functions.js
+++ 03_BASICS/01_functions.js
@@ -23,4 +23,33 @@ function loginUserMsg(username){
     return `${username} just logged in`
 }
 //      console.log(loginUserMsg("bhavesh"));
-console.log(loginUserMsg());// ->  undefined just logged in
+// console.log(loginUserMsg());// ->  undefined just logged in
+
+
+
+function calculateCartPrice(...num1){ // ... as Rest Operator
+    return num1
+}
+// console.log((calculateCartPrice(200, 400, 1000)));
+
+const user = {
+  username : "abc",
+  price : 999
+}
+
+function handleObject(anyobject){
+    console.log(`Username is : ${anyobject.username} and price is : ${anyobject.price}`);
+}
+
+// handleObject(user)
+// handleObject({username : "xyz", price : 1999})//passing direct object
+
+//===========================
+
+const myNewArray = [200, 400, 100, 600]
+function getValue(getArray){
+    return getArray[1]
+}
+
+// console.log(getValue(myNewArray));
+console.log(getValue([200,800,500]));
