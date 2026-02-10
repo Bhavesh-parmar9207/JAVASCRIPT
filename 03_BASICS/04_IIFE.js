87acf4966d7b74ab17ca88b6a13024aaccd6089a
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-02-10 23:37:00 +0530

    IIFE

---
+++ 03_BASICS/04_IIFE.js
@@ -0,0 +1,15 @@
+//Immediately Invoked Function Expressions (IIFE) - To remove polution of global scope
+
+(function chai(){
+//Named IIFE
+    console.log('DB CONNECTED');
+})();
+
+// hERE ()() first for function second to execution call 
+
+//sometimes if there is multiple iife implemented then we need end first function with ;
+
+((name) => {
+    //simple IIFE
+    console.log(`DB CONNECTED AGAIN ${name}`);
+})('Bhavesh')
