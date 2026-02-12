a623f4e9a6a9b9b2d5b5b6e78225bbb5ef2cc96c
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-02-12 23:46:52 +0530

    simple switch case

---
+++ 04_CONTROL_FLOW/switch.js
@@ -0,0 +1,32 @@
+//Basic Leyout
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+//moth check
+const month = 3
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+//if it's match then it never runs default case
+    default:
+        console.log("default case match");
+    break;
+}
