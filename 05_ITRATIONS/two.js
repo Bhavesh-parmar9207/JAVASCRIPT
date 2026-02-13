76a2de4725a38e3a0c2eb6714e208cb7f9201f94
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-02-13 23:35:02 +0530

    while & do while

---
+++ 05_ITRATIONS/two.js
@@ -0,0 +1,28 @@
+//while loop
+
+let index = 0
+while (index <= 10 ) {
+    // console.log(`value is : ${index}`);
+    index = index + 2
+}
+
+
+
+let myArray = ["Batman","Spiderman","Superman"]
+let arr =0
+// console.log(myArray.length);
+
+while (arr < myArray.length ) {
+    // console.log(`value is : ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+
+
+//do while => first it will iterates and then checks the condtion
+let score = 11
+do {
+    console.log(`score is : ${score}`);
+    score++
+} while (score <= 10);
