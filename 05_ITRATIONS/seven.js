a05ab83387bb9316aa61a299c9886c9187b6e415
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-02-14 23:55:03 +0530

    map and chaining

---
+++ 05_ITRATIONS/seven.js
@@ -0,0 +1,9 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//map
+// const newNums = nums.map((num) => { return num + 10 })//auto return the values 
+// console.log(newNums);
+
+//chaining => in this every chain's results are passed to next
+const newNums = nums.map((num) => num * 10).map((num) => num + 1).filter((num) => num >= 20)
+console.log(newNums);
